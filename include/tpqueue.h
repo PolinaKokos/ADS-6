d2e8f516bd43592cd495a394ee007d4ec2e07f78
PolinaKokos <kosikpm@gmail.com>
2026-04-26 21:27:06 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,86 +1,74 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H
-#define INCLUDE_TPQUEUE_H
-
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+struct SYM {
+    char ch;
+    int prior;
+};
 template<typename T>
 class TPQueue {
-private:
+ private:
     struct Node {
         T data;
         Node* next;
-        Node(const T& value) : data(value), next(nullptr) {}
+        explicit Node(const T& value) : data(value), next(nullptr) {}
     };
-    
     Node* head;
     Node* tail;
     int size;
-    
-public:
+ public:
     TPQueue() : head(nullptr), tail(nullptr), size(0) {}
-    
+
     ~TPQueue() {
         while (!isEmpty()) {
             pop();
         }
     }
-    
     void push(const T& item) {
         Node* newNode = new Node(item);
-        
         if (isEmpty()) {
             head = tail = newNode;
             size++;
             return;
         }
-        
         if (item.prior > head->data.prior) {
             newNode->next = head;
             head = newNode;
             size++;
             return;
         }
-        
         Node* current = head;
-        while (current->next != nullptr && current->next->data.prior >= item.prior) {
+        while (current->next != nullptr &&
+               current->next->data.prior >= item.prior) {
             current = current->next;
         }
-        
         newNode->next = current->next;
         current->next = newNode;
-        
         if (newNode->next == nullptr) {
             tail = newNode;
         }
-        
         size++;
     }
-    
     T pop() {
         if (isEmpty()) {
             throw "Queue is empty";
         }
-        
         Node* temp = head;
         T result = head->data;
         head = head->next;
-        
         if (head == nullptr) {
             tail = nullptr;
         }
-        
         delete temp;
         size--;
         return result;
     }
-    
     bool isEmpty() const {
         return head == nullptr;
     }
-    
     int getSize() const {
         return size;
     }
-    
     T front() const {
         if (isEmpty()) {
             throw "Queue is empty";
@@ -88,5 +76,4 @@ public:
         return head->data;
     }
 };
-
-#endif // INCLUDE_TPQUEUE_H
+#endif  // INCLUDE_TPQUEUE_H_
