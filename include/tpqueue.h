fb0526a2e9210e65e96e46f405e3775c1aa68b01
PolinaKokos <kosikpm@gmail.com>
2026-04-26 21:39:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,12 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+
 struct SYM {
     char ch;
     int prior;
 };
+
 template<typename T>
 class TPQueue {
  private:
@@ -13,9 +15,11 @@ class TPQueue {
         Node* next;
         explicit Node(const T& value) : data(value), next(nullptr) {}
     };
+
     Node* head;
     Node* tail;
     int size;
+
  public:
     TPQueue() : head(nullptr), tail(nullptr), size(0) {}
 
@@ -24,51 +28,65 @@ class TPQueue {
             pop();
         }
     }
+
     void push(const T& item) {
         Node* newNode = new Node(item);
+
         if (isEmpty()) {
             head = tail = newNode;
             size++;
             return;
         }
+
         if (item.prior > head->data.prior) {
             newNode->next = head;
             head = newNode;
             size++;
             return;
         }
+
         Node* current = head;
         while (current->next != nullptr &&
                current->next->data.prior >= item.prior) {
             current = current->next;
         }
+
         newNode->next = current->next;
         current->next = newNode;
+
         if (newNode->next == nullptr) {
             tail = newNode;
         }
+
         size++;
     }
+
     T pop() {
         if (isEmpty()) {
             throw "Queue is empty";
         }
+
         Node* temp = head;
         T result = head->data;
         head = head->next;
+
         if (head == nullptr) {
             tail = nullptr;
         }
+
         delete temp;
         size--;
         return result;
     }
+
     bool isEmpty() const {
         return head == nullptr;
     }
+
     int getSize() const {
         return size;
     }
+
     T front() const {
         if (isEmpty()) {
             throw "Queue is empty";
@@ -76,4 +94,5 @@ class TPQueue {
         return head->data;
     }
 };
+
 #endif  // INCLUDE_TPQUEUE_H_
